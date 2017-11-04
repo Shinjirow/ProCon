0e2ed090a04a604caf920b2d4ba6b736d63f299e
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2017-11-04 20:57:42 +0900

    ABC077にでる

---
+++ AtCoder/ABC077/probC.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <stack>
+#include <queue>
+
+#define REP(i,a,n) for(int i=a;i<n;++i)
+#define rep(i,n) REP(i,0,n)
+#define vsort(v) sort(v.begin(),v.end())
+#define asort(a,n) sort(a, a + n)
+#define vi vector<int>
+#define vd vector<double>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
