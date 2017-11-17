455acd4d5f97369b12b573d708743880e8b8d79e
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2017-11-17 18:38:08 +0900

    Tera2017のフォルダの中に問題数分ファイルを作りました

---
+++ TeraCoder/2017/probJ.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+#include <queue>
+
+#define REP(i,a,n) for(int i=a;i<n;++i)
+#define rep(i,n) REP(i,0,n)
+#define vsort(v) sort(v.begin(),v.end())
+#define asort(a,n) sort(a, a + n)
+#define vi vector<int>
+#define vd vector<double>
+
+//ここからTeraCoder向けdefine
+#define ok cout << "OK" << "\n"
+#define ng cout << "NG" << "\n"
+#define case(i) cout << "Case #" << i + 1 << ":\n"
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+
+    return 0;
+}
