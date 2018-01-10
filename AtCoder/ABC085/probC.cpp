c6a04c712b9a416e5fcf96d637eb0359fb156696
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-01-10 12:23:29 +0900

    ABC085Bまでやった

---
+++ AtCoder/ABC085/probC.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+#include <queue>
+
+#define REP(i,a,n) for(int i=a;i<n;++i)
+#define rep(i,n) REP(i,0,n)
+#define vsort(v) sort(v.begin(),v.end())
+#define asort(a,n) sort(a, a + n)
+#define vi vector<int>
+#define vd vector<double>
+
+//ここからAtCoder向けdefine
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
+
+using namespace std;
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int osatsu[3] = {10000, 5000, 1000};
+    int n, y;
+    cin >> n >> y;
+    
+
+    return 0;
+}
