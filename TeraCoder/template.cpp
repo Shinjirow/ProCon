f1a47153deac0c3c02fdc0687da32162239b1771
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-01-27 23:13:39 +0900

    ちょっと更新

--- TeraCoder/template.cpp
+++ TeraCoder/template.cpp
@@ -8,11 +8,13 @@
 #include <cstring>
 #include <stack>
 #include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
 
 #define REP(i,a,n) for(int i=a;i<n;++i)
 #define rep(i,n) REP(i,0,n)
-#define vsort(v) sort(v.begin(),v.end())
-#define asort(a,n) sort(a, a + n)
+#define all(e) e.begin(), e.end()
 #define vi vector<int>
 #define vd vector<double>
 
@@ -21,6 +23,11 @@
 #define ng cout << "NG" << "\n"
 #define case(i) cout << "Case #" << i + 1 << ":\n"
 #define import(n) int n;cin >> n;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const int INF = ~(1 << 31);
 
 using namespace std;
 
