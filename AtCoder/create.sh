efb96b15ad095b1eab9d1967a3ffca80a970fd53
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-01-27 16:58:34 +0900

    update, よけいな一文をけした

--- AtCoder/create.sh
+++ AtCoder/create.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-declare -a array=()
 declare -a array=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z")
 
 echo "Contest name = "
