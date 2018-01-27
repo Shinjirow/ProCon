f99abebe2633a1cc0683a8f6049237b79d7236ab
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-01-27 16:40:53 +0900

    update, cpをfor文でくるんだ

--- AtCoder/create.sh
+++ AtCoder/create.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
 
+declare -a array=()
+declare -a array=("A" "B" "C" "D" "E" "F" "G" "H" "I" "J" "K" "L" "M" "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z")
+
+echo "Contest name = "
 read NAME
 
 mkdir -p ./${NAME}
 
 if [ ${NAME:0:3} = "ABC" ]; then
-    cp ./template/template.cpp ./${NAME}/probA.cpp
-    cp ./template/template.cpp ./${NAME}/probB.cpp
-    cp ./template/template.cpp ./${NAME}/probC.cpp
-    cp ./template/template.cpp ./${NAME}/probD.cpp
+    for ((i = 0;i < 4;i++)) {
+        cp ./template/template.cpp ./${NAME}/prob${array[i]}.cpp
+    }
 elif [ ${NAME:0:3} = "ARC" ]; then
-    cp ./template/template.cpp ./${NAME}/probC.cpp
-    cp ./template/template.cpp ./${NAME}/probD.cpp
-    cp ./template/template.cpp ./${NAME}/probE.cpp
-    cp ./template/template.cpp ./${NAME}/probF.cpp
+    for ((i = 2;i < 6;i++)) {
+        cp ./template/template.cpp ./${NAME}/prob${array[i]}.cpp
+    }
+else
+    echo "Number of Question = "
+    read QUESTIONS
+    for ((i = 0;i < ${QUESTIONS};i++)) {
+        cp ./template/template.cpp ./${NAME}/prob${array[i]}.cpp
+    }
 fi
 cp ./template/Makefile ./${NAME}
 touch ./${NAME}/input.txt
