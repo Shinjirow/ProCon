4c407038c8b267554f45feff1578fa0e25a13882
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-02-02 17:49:37 +0900

    結果的に写経になったけどACした、何となくグラフのやり方を知った気がする(わかってない)

--- AtCoder/ARC090/probD.cpp
+++ AtCoder/ARC090/probD.cpp
@@ -29,68 +29,69 @@ const int INF = ~(1 << 31);
 
 using namespace std;
 
+bool already[100001];
+int num[100001];
+
+vector<pair<int, int> > graph[100001];
+// 自分用メモ
+// vectorの配列、配列の各要素がfrom
+// その中にtoとコストをpairで管理
+bool ng = false;
+
+void dfs(int n, int cost){
+    //cerr << "from = " << n << ", currentcost = " << cost << endl;
+    /*if(already[n]) cerr << "true" << endl;
+    else cerr << "false" << endl;*/
+    if(already[n]){
+        //cerr << "num = " << num[n] << ", cost = " << cost << endl;
+        if(num[n] != cost){
+            ng = true;
+            //cerr << "flag has standed" << endl;
+        }
+        return;
+    }
+    already[n] = true;
+    num[n] = cost;
+    //cerr << "size = " << graph[n].size() << endl;
+    rep(i, graph[n].size()){
+        pair<int, int> p = graph[n][i];
+        //cerr << "to = " << p.first << ", cost = " << p.second << endl;
+        dfs(p.first, p.second + cost);
+    }
+}
+
+
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
     int n, m;
     cin >> n >> m;
-    int hito[n + 1];
-    bool deta[n + 1];
-    REP(i, 1, n + 1){ deta[i] = false;}
-    //REP(i, 1, n + 1){ cerr << deta[i] << endl; }
-    //memset(hito, -1, sizeof(hito));
-    //REP(i, 1, n + 1){ cerr << hito[i] << endl; }
-    vector<pair<pair<int, int>, int> > vec(m);
     int l, r, d;
     rep(i, m){
         cin >> l >> r >> d;
-        vec[i] = make_pair(make_pair(l, r), d);
-    }
-    if(m == 0) { yes; return 0; }
-    int maxVal = -50;
-    int minVal = 50;
-    hito[vec[0].first.first] = 0;
-    deta[vec[0].first.first] = true;
-    rep(i, m){
-        //cerr << "----------" << endl;REP(i, 1, n + 1){ cerr << hito[i] << ' ' << deta[i] << endl; }
-        if(!deta[vec[i].first.second] || !deta[vec[i].first.first]){
-            if(!deta[vec[i].first.second] && !deta[vec[i].first.first]){
-                REP(j, i, m){
-                    if(vec[j].first.second == vec[i].first.first){
-                        if(!deta[vec[j].first.first]){ continue; }
-                        else{
-                            hito[vec[j].first.second] = hito[vec[j].first.first] + vec[j].second;
-                            break;
-                        }
-                    }
-                }
-            }
-            if(!deta[vec[i].first.second]){
-                hito[vec[i].first.second] = hito[vec[i].first.first] + vec[i].second;
-                //maxVal = max(hito[vec[i].first.first] + vec[i].second, maxVal);
-                //minVal = min(hito[vec[i].first.first], minVal);
-                deta[vec[i].first.second] = true;
-            }else{
-                hito[vec[i].first.first] = hito[vec[i].first.second] - vec[i].second;
-                //maxVal = max(hito[vec[i].first.second], maxVal);
-                //minVal = min(hito[vec[i].first.second] - vec[i].second, minVal);
-                deta[vec[i].first.first] = true;
-            }
-        }else{
-            if(hito[vec[i].first.second] == hito[vec[i].first.first] + vec[i].second){
-                continue;
-            }else{
-                no;
-                return 0;
-            }
-        }
-        //cerr << maxVal << ' ' << minVal << endl;
-        //if(maxVal - minVal > 1000000000){ no; return 0;}
+        l--;
+        r--;
+        graph[l].push_back(make_pair(r, d));
+        graph[r].push_back(make_pair(l, -d));
     }
 
-    // REP(i, 1, n + 1){
-    //     cerr << hito[i] << endl;
-    // }
+    /*
+    rep(i, n){
+        rep(j, graph[i].size()){
+            cerr << "from = " << i << ", to = " << graph[i][j].first << ", cost = " << graph[i][j].second << endl;
+        }
+    }*/
+
+    memset(already, false, sizeof(already));
+    rep(i, n){
+        if(already[i]) continue;
+        dfs(i, 0);
+
+        if(ng){
+            no;
+            return 0;
+        }
+    }
     yes;
 
     return 0;
