cf06291447f9a8797d8852bc6882e376b70e274d
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-03-15 02:02:13 +0900

    BFS書けるようになるために解いた

--- AtCoder/ABC088/probD.cpp
+++ AtCoder/ABC088/probD.cpp
@@ -35,11 +35,56 @@ using ull = unsigned long long;
 using vi = vector<int>;
 using pint = pair<int,int>;
 
-const int INF = ~(1 << 31);
+const int INF = (1 << 30);
+
+vi grid[51];
+
+vi dis[51];
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+
+int bfs(int h, int w){
+    pint s = mp(0, 0);
+    pint g = mp(h - 1, w - 1);
+    queue<pint> queue;
+    rep(i, h){rep(j, w){dis[i].pb(INF);}}
+
+    queue.push(s);
+    dis[s.fs][s.sc] = 0;
+
+    while(queue.size()){
+        pint p = queue.front(); queue.pop();
+        if(p.fs == g.fs && p.sc == g.sc){ break; }
+
+        rep(i, 4){
+            pint next = mp(p.fs + dx[i], p.sc + dy[i]);
+
+            if(0 <= next.fs && next.fs < h && 0 <= next.sc && next.sc < w && grid[next.fs][next.sc] != 1 && dis[next.fs][next.sc] == INF){
+                queue.push(next);
+                dis[next.fs][next.sc] = dis[p.fs][p.sc] + 1;
+            }
+        }
+    }
+    return dis[g.fs][g.sc];
+}
 
 int main(){
     cin.tie(0);
     ios::sync_with_stdio(false);
+    int h, w;
+    cin >> h >> w;
+    char c;
+    int masu = 0;
+    rep(i, h){
+        rep(j, w){
+            cin >> c;
+            if(c == '.'){grid[i].pb(0);masu++;}
+            else{grid[i].pb(1);}
+        }
+    }
+    int n = bfs(h, w);
+    if(n == INF){ cout << -1 << endl; }
+    else{ cout << masu - n - 1 << endl; }
 
     return 0;
 }
