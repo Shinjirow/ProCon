29afd483ade622c275444eeaff9ceab7dc9a26fa
Shinjirow <g1644204@cc.kyoto-su.ac.jp>
2018-03-29 17:13:18 +0900

    ちょっと解いた

---
+++ AtCoder/ABC092/probC.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <utility>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#define REP(i,a,n) for(int i=a;i<n;++i)
+#define rep(i,n) REP(i,0,n)
+#define REV(i,a,n) for(int i=n;i>=a;--i)
+#define all(e) e.begin(),e.end()
+#define rall(e) e.rbegin(),e.rend()
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define mod 1000000007
+#define show(n) cerr<<#n<<" = "<<n<<endl
+#define shows(n) for(auto z:n){cerr<<z<<", ";}cerr<<endl
+
+//ここからAtCoder向けdefine
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<int>;
+using pint = pair<int,int>;
+
+const int INF = (1 << 30);
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+    vi vec(n+2);
+    vec[0] = 0;
+    REP(i, 1, n+1) cin >> vec[i];
+    vec[n+1] = 0;
+
+    ll ans = 0;
+    rep(i, n+1){
+        ans += abs(vec[i+1] - vec[i]);
+    }
+    cout << ans << endl;
+    rep()
+
+    return 0;
+}
